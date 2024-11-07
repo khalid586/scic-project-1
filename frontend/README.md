f425623a5ce37eb251cfc2566ad6296ed0e4e06c
khalid <khalidcomilla58@gmail.com>
2024-11-08 02:32:33 +0600

    basic setup

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+Personal starter kit for react apps
---
+++ frontend/src/Root.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Footer from './Components/Footer'
+// import Banner from './Components/Banner'
+// import Navbar from './Components/Navbar'
+import { Outlet } from 'react-router-dom'
+
+function Root() {
+  return (
+    <div className='font-custom'>
+
+      <div className='md:mx-10 lg:mx-20'>
+         {/* <Navbar></Navbar>
+
+         */}
+          <div className='min-h-[90vh]'>
+              <Outlet></Outlet>
+          </div>
+      </div>
+        <Footer></Footer>
+    </div>
+  )
+  
+}
+
+export default Root
